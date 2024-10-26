036006ed9951b6828f6eb1445091187d3b52b7e7
Monand <monandszy@tuta.io>
2024-10-26 20:20:52 +0200

    feat: 425

--- src/test/java/code/SomeTest.java
+++ src/test/java/code/SomeTest.java
@@ -65,7 +65,7 @@ class SomeTest {
         .filter(Files::isRegularFile)
         .map(e -> e.getFileName().toString().split("\\.")[0])
         .sorted()
-        .skip(367)
+        .skip(419)
 //        .limit(20)
         .forEach(this::translate);
 
@@ -74,7 +74,7 @@ class SomeTest {
 
   @Test
   void translateOne() {
-    translate("397");
+    translate("420");
   }
 
   @SneakyThrows
@@ -92,9 +92,9 @@ class SomeTest {
               Convert sentences to past tense to give the narrative a story-like, polished feel, except for descriptions.
               No markdown or stylistic formatting—simply return the draft English text with title.
           """ +
-        "!!! ADDITIONAL DATA: The draft is around 150 lines, Raven is a woman, Erwin is a woman, Misha is a woman. Ainar is a woman " +
+        "!!! ADDITIONAL DATA: The draft is around 150-250 lines, Raven is a woman, Erwin is a woman, Misha is a woman. Ainar is a woman " +
         "DO NOT CONFUSE THE MAIN CHARACTER (man in first person) FOR THEM. " +
-        "The term '악령 (aknyeong)' translates to 'Evil Spirit' !!!" +
+        "The term '악령 (aknyeong)' translates to 'Evil Spirit', '균열 (gyeonyeol)' to 'rift' !!!" +
         "!!! DRAFT: [[[[%s]]] !!!. " +
         "!!! CONTENT FOR CONTEXT so that you understand better: [[[%s]]] !!!"
     ).formatted(Files.readString(Path.of("_scraped/%s.txt".formatted(chapter))),
